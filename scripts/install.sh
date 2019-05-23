be25660919116f683fd48325de90212e02725ed4
Ilya Kuzovkin <ilya.kuzovkin@gmail.com>
2019-05-23 11:41:16 +1000

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,43 +1,52 @@
 #!/usr/bin/env bash
 
-# Install python3.6
-cd /opt
-sudo wget https://www.python.org/ftp/python/3.6.3/Python-3.6.3.tgz
-sudo tar -xvf Python-3.6.3.tgz
-cd Python-3.6.3
-sudo ./configure
-sudo make 
-sudo make install 
+# this assumes the script is executed from under `scripts` directory
+# (should we move `install.sh` to the repository root? will be cleaner)
+cd ..
+$OFFWORLD_GYM_ROOT = `pwd`
 
-# Create virtual env
-sudo pip3.6 install virtualenv
-virtualenv -p python3.6 ~/ve/py36
-echo "source ~/ve/py36/bin/activate" >> ~/.bashrc
-source ~/.bashrc
+# make sure we have Python 3.5
+sudo apt install python3.5 python3.5-dev
 
-# Python packages
+# create virtual environment
+sudo apt install virtualenv
+mkdir ~/ve
+virtualenv -p python3.5 ~/ve/py35gym
+source ~/ve/py35gym/bin/activate
+pip install --upgrade pip
+
+# (we will drop this)
+#echo "source ~/ve/py36/bin/activate" >> ~/.bashrc
+#source ~/.bashrc
+
+# intall Python packages
 pip install numpy
 pip install tensorflow-gpu
 pip install keras
 pip install opencv-python
-
-# python ros 
 pip install catkin_pkg
 pip install empy
 pip install defusedxml
 pip install rospkg
 pip install matplotlib
 pip install netifaces
-
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt update
-sudo apt install python3.6-dev
 pip install regex
 pip install psutil
 
-# ros workspac
-mkdir -p ~/catkin_ws/src
-cd ~/catkin_src
+# install additional ROS packages
+sudo apt install ros-kinetic-grid-map ros-kinetic-frontier-exploration ros-kinetic-ros-controllers -y
+
+# build Python 3.5 version of catkin *without* installing it system-wide
+mkdir $OFFWORLD_GYM_ROOT/assets
+cd $OFFWORLD_GYM_ROOT/assets
+git clone https://github.com/ros/catkin.git -b kinetic-devel
+cd $OFFWORLD_GYM_ROOT/assets/catkin
+mkdir build && cmake .. && make
+
+# build ROS workspace
+cd $OFFWORLD_GYM_ROOT/offworld_gym/envs/gazebo/catkin_ws/src
+$OFFWORLD_GYM_ROOT/assets/catkin/bin/catkin_init_workspace
+
 git clone https://github.com/ros/xacro.git -b kinetic-devel
 git clone https://github.com/ros/ros.git -b kinetic-devel
 git clone https://github.com/ros/catkin.git -b kinetic-devel
@@ -53,5 +62,16 @@ git clone https://github.com/ros/geometry.git -b indigo-devel
 git clone https://github.com/ros/geometry2.git -b indigo-devel
 git clone https://github.com/ros-simulation/gazebo_ros_pkgs.git -b kinetic-devel
 git clone https://github.com/ros-controls/ros_control.git -b kinetic-devel
+git clone https://github.com/ros/dynamic_reconfigure.git -b master
+git clone https://github.com/husarion/rosbot_description.git -b devel
 
-./install_environment.sh
+cd ..
+$OFFWORLD_GYM_ROOT/assets/catkin/bin/catkin_make -j1
+
+# integrate the new environment into the system
+echo "source $OFFWORLD_GYM_ROOT/offworld_gym/envs/gazebo/catkin_ws/devel/setup.bash --extend" >> ~/.bashrc
+echo "export GAZEBO_MODEL_PATH=$OFFWORLD_GYM_ROOT/offworld_gym/envs/gazebo/catkin_ws/src/gym_offworld_monolith/models:$GAZEBO_MODEL_PATH" >> ~/.bashrc
+source ~/.bashrc
+
+echo "Installation complete!"
+echo "To test it run 'roslaunch gym_offworld_monolith env_bringup.launch'"
