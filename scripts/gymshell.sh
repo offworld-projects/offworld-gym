0f10b8a281fa5f40250f5d24ede26104df1f5fd8
Ilya Kuzovkin <ilya.kuzovkin@gmail.com>
2019-06-19 13:46:11 -0700

    removed gymshell.sh as it is generated during install

--- scripts/gymshell.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-source ~/ve/py35gym/bin/activate
-unset PYTHONPATH
-source /opt/ros/kinetic/setup.bash
-source /home/ilya/offworld/Code/offworld-gym/offworld_gym/envs/gazebo/catkin_ws/devel/setup.bash --extend
-export GAZEBO_MODEL_PATH=/home/ilya/offworld/Code/offworld-gym/offworld_gym/envs/gazebo/catkin_ws/src/gym_offworld_monolith/models:
-export PYTHONPATH=~/ve/py35gym/lib/python3.5/site-packages:$PYTHONPATH
-
-
--- scripts/install.sh
+++ scripts/install.sh
@@ -92,7 +92,7 @@ echo "unset PYTHONPATH" >> $OFFWORLD_GYM_ROOT/scripts/gymshell.sh
 echo "source /opt/ros/kinetic/setup.bash" >> $OFFWORLD_GYM_ROOT/scripts/gymshell.sh
 echo "source $OFFWORLD_GYM_ROOT/offworld_gym/envs/gazebo/catkin_ws/devel/setup.bash --extend" >> $OFFWORLD_GYM_ROOT/scripts/gymshell.sh
 echo "export GAZEBO_MODEL_PATH=$OFFWORLD_GYM_ROOT/offworld_gym/envs/gazebo/catkin_ws/src/gym_offworld_monolith/models:$GAZEBO_MODEL_PATH" >> $OFFWORLD_GYM_ROOT/scripts/gymshell.sh
-echo "export PYTHONPATH=~/ve/py35gym/lib/python3.5/site-packages:$PYTHONPATH" >> $OFFWORLD_GYM_ROOT/scripts/gymshell.sh
+echo 'export PYTHONPATH=~/ve/py35gym/lib/python3.5/site-packages:$PYTHONPATH' >> $OFFWORLD_GYM_ROOT/scripts/gymshell.sh
 echo "export OFFWORLD_GYM_ROOT=$OFFWORLD_GYM_ROOT" >> $OFFWORLD_GYM_ROOT/scripts/gymshell.sh
 chmod +x $OFFWORLD_GYM_ROOT/scripts/gymshell.sh
 
