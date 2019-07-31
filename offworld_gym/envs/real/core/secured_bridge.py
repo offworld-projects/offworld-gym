5bc389c5013d1df6a2f9c62dcbb12ad413d209bc
Ashish Kumar <akuma119@asu.edu>
2019-07-31 16:26:42 -0700

    Added HTTPS based communication

--- offworld_gym/envs/real/core/secured_bridge.py
+++ offworld_gym/envs/real/core/secured_bridge.py
@@ -13,6 +13,7 @@ __status__      = "Development"
 from offworld_gym import logger
 import numpy as np
 import requests
+import os 
 
 # offworld gym
 from offworld_gym.envs.real.config import settings
@@ -34,13 +35,14 @@ class SecuredBridge(metaclass=Singleton):
         self.server_ip = self.settings_dict["gym_server"]["server_ip"]
         self.secured_port = self.settings_dict["gym_server"]["secured_port"]
         self._action_counter = 0
+        self._certificate = False #os.path.join(os.path.dirname(os.path.realpath(__file__)), "../certs/gym_monolith/certificate.pem") #TODO find out why doesn't the certificate work, unverified certs can cause mitm attack
 
     def get_last_heartbeat(self):
         """Return the last heartbeat value
         """
         req = HeartBeatRequest()
-        api_endpoint = "http://{}:{}/{}".format(self.server_ip, self.secured_port, HeartBeatRequest.URI)
-        response = requests.post(url = api_endpoint, json = req.to_dict()) 
+        api_endpoint = "https://{}:{}/{}".format(self.server_ip, self.secured_port, HeartBeatRequest.URI)
+        response = requests.post(url = api_endpoint, json = req.to_dict(), verify=self._certificate) 
         response_json = json.loads(response.text)
         if not DEBUG: logger.debug("Heartbeat  : {}".format(response_json['heartbeat']))
         return response_json['heartbeat']
@@ -52,9 +54,9 @@ class SecuredBridge(metaclass=Singleton):
         if not DEBUG: logger.debug("Start executing action {}, count : {}.".format(action_type.name, str(self._action_counter)))
         
         req = ActionRequest(action_type=action_type, channel_type=channel_type)
-        api_endpoint = "http://{}:{}/{}".format(self.server_ip, self.secured_port, ActionRequest.URI)
+        api_endpoint = "https://{}:{}/{}".format(self.server_ip, self.secured_port, ActionRequest.URI)
 
-        response = requests.post(url = api_endpoint, json = req.to_dict()) 
+        response = requests.post(url = api_endpoint, json = req.to_dict(), verify=self._certificate) 
         response_json = json.loads(response.text)
 
         reward = int(response_json['reward'])
@@ -76,8 +78,8 @@ class SecuredBridge(metaclass=Singleton):
         if DEBUG: logger.debug("Waiting for reset done from the server.")       
         
         req = ResetRequest(channel_type=channel_type)
-        api_endpoint = "http://{}:{}/{}".format(self.server_ip, self.secured_port, ResetRequest.URI)
-        response = requests.post(url = api_endpoint, json = req.to_dict()) 
+        api_endpoint = "https://{}:{}/{}".format(self.server_ip, self.secured_port, ResetRequest.URI)
+        response = requests.post(url = api_endpoint, json = req.to_dict(), verify=self._certificate) 
         response_json = json.loads(response.text)
 
         state = json.loads(response_json['state'])
--- offworld_gym/tests/test_client_server.py
+++ offworld_gym/tests/test_client_server.py
@@ -1,3 +1,4 @@
+from matplotlib import pyplot as plt
 from offworld_gym.envs.real.core.secured_bridge import SecuredBridge
 from offworld_gym.envs.common.actions import FourDiscreteMotionActions
 from offworld_gym.envs.common.channels import Channels
@@ -6,8 +7,9 @@ mc = SecuredBridge()
 
 print(mc.get_last_heartbeat())
 print("Connection established.")
-state,reward,done = mc.perform_action(FourDiscreteMotionActions.FORWARD, Channels.DEPTH_ONLY)
+state,reward,done = mc.perform_action(FourDiscreteMotionActions.FORWARD, Channels.RGB_ONLY)
 print(state.shape, reward, done)
-
+plt.imshow(state[0])
+plt.show()
 state = mc.perform_reset(Channels.DEPTH_ONLY)
 print(state.shape)
