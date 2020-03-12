1126c589b77e7454b14104ced51ebabceeed4488
JB Lanier <john.lanier@offworld.ai>
2020-03-12 14:43:39 -0700

    debugging docker script

--- offworld_gym/envs/gazebo/remote/remote_env_server.py
+++ offworld_gym/envs/gazebo/remote/remote_env_server.py
@@ -87,7 +87,7 @@ if __name__ == '__main__':
     random_init = parse_random_init_from_environ()
 
     print("Starting env...")
-    # env: GazeboGymEnv = env_class(channel_type=channel_type, random_init=random_init)
+    env: GazeboGymEnv = env_class(channel_type=channel_type, random_init=random_init)
     print("Env fully initialized")
 
     # GRPC Server
